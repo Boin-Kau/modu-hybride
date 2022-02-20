ed600ecb5fa51451b7773420aa0e74afa077b753
MinJoo-Park01 <supermp@hufs.ac.kr>
2022-02-21 01:02:12 +0900

    feat: 계좌카드

--- src/views/payment/card.js
+++ src/views/payment/card.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-const card = () => {
+import ic_pay_cardtab from "../../assets/ic_pay_cardtab.svg";
+
+const Card = () => {
   return (
-    <div>card</div>
-  )
-}
+    <Container
+    className="spoqaBold"
+      style={{
+        backgroundImage: `url(${ic_pay_cardtab})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div style={{display:'flex', flexDirection:'column', width:'80%', height:'40%', justifyContent:'space-between', marginTop:'20%'}}>
+        <span style={{fontSize:'0.875rem'}}>신한카드</span>
+        <span style={{fontSize:'1.5625rem'}}>1234********5678</span>
+      </div>
+    </Container>
+  );
+};
 
-export default card
+const Container = styled.div`
+  width: 94%;
+  height: 10.625rem;
+  border-radius: 8px;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.04);
+  border: dashed 0.4px #bcbcbc;
+  background-color: #f5f5f5;
+  margin-top: 1.125rem;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+
+  span{
+    color:#fff;
+    text-align: left;
+  }
+`;
+
+export default Card;
