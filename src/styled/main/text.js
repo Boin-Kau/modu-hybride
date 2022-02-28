ee9d5e93f9131e1ce4e79b208b6ebbfba54542b3
MinJoo-Park01 <supermp@hufs.ac.kr>
2022-03-01 01:08:49 +0900

    fix : 텍스트 컴포넌트 수정

--- src/styled/main/text.js
+++ src/styled/main/text.js
@@ -4,8 +4,8 @@ export const MainText = styled.div`
 
   font-size: 1.25rem;
   line-height: 1.6;
-  margin: 1rem 5.375rem 1.5rem 0;
-
+  margin: 1rem 0 1.5rem 0;
+  
   .yellowText{
     color: #ffbc26;
   }
