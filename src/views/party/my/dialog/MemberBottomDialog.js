23f6706593b39a348ee9ad8b6cda4a33d537815f
kt999 <kt999@softsquared.com>
2022-04-09 22:43:54 +0900

    Update: 해지팝업 분기처리

--- src/views/party/my/dialog/MemberBottomDialog.js
+++ src/views/party/my/dialog/MemberBottomDialog.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { MemberBottomDialogCloseAction, PartyDeleteConfirmDialogOpenAction } from "../../../../reducers/party/popup";
 import { BottomDialogDiv, BottomDialogWrap } from "../../../../styled/shared/wrap";
 
-const MemberBottomDialog = ({roomStatus}) => {
+const MemberBottomDialog = ({ userStatus }) => {
 
   const dispatch = useDispatch();
   const { memberBottomDialogStatus } = useSelector(state => state.party.popup);
@@ -16,12 +16,12 @@ const MemberBottomDialog = ({roomStatus}) => {
     dispatch(MemberBottomDialogCloseAction);
   };
 
-  return(
+  return (
     <BottomDialogWrap openStatus={memberBottomDialogStatus}>
       <BottomDialogDiv>
         <div className="btn_style margin_bottom">
           {/* 해지하기 or 해지 취소하기 */}
-          <div onClick={openConfirmDialog} className="one_btn gray_border_bottom">{roomStatus === 'RESERVED' ? '해지 취소하기':'해지하기'}</div>
+          <div onClick={openConfirmDialog} className="one_btn gray_border_bottom">{userStatus === 'RESERVED' ? '해지 취소하기' : '해지하기'}</div>
           <div className="one_btn">신고하기</div>
         </div>
         <div onClick={closeDialog} className="btn_style one_btn">취소</div>
