5e7f8b561827bb9372913db9c098c5b319bfbd5f
kt999 <kt999@softsquared.com>
2022-04-11 01:29:18 +0900

    Add: 탈퇴/해지된 파티 에러처리

--- src/views/party/my/myPartyDetail.js
+++ src/views/party/my/myPartyDetail.js
@@ -105,6 +105,11 @@ const MyPartyDetail = () => {
   }, []);
 
   useEffect(() => {
+
+    setFinishPopupTitle("");
+    setFinishPopupSubTitle("");
+    setFinishPopupStatus(false);
+
     getPartyDetail();
   }, [partyIdx])
 
@@ -139,14 +144,26 @@ const MyPartyDetail = () => {
 
   // Function 
   const getPartyDetail = async () => {
+
+    if (!partyIdx || partyIdx === 0) return
     // 파티 상세 조회 
     const partyDetailUri = `/party/${partyIdx}`;
     const partyDetailData = await customApiClient('get', partyDetailUri);
 
     // Server Error
-    if (!partyDetailData) { return };
+    if (!partyDetailData) {
+      setFinishPopupTitle("탈퇴 혹은 해지된 파티입니다");
+      setFinishPopupSubTitle("해당 파티는 탈퇴 혹은 해지된 파티입니다.\n종료됨 파티탭을 확인해주세요.");
+      setFinishPopupStatus(true);
+      return
+    };
     // Validation 
-    if (partyDetailData.statusCode !== 200) { return };
+    if (partyDetailData.statusCode !== 200) {
+      setFinishPopupTitle("탈퇴 혹은 해지된 파티입니다");
+      setFinishPopupSubTitle("해당 파티는 탈퇴 혹은 해지된 파티입니다.\n종료됨 파티탭을 확인해주세요.");
+      setFinishPopupStatus(true);
+      return
+    };
     console.log('API 호출 성공 :', partyDetailData);
 
     setResult(partyDetailData.result);
