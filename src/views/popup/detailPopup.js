010935d49f41e69f1b54d473dc0a988b1e75cc27
MinJoo-Park01 <supermp@hufs.ac.kr>
2022-04-27 15:48:40 +0900

    fix:카드등록 버그 수정

---
+++ src/views/popup/detailPopup.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { DangerWrapPopup } from "../../styled/shared";
+
+const DetailPopup = (status) => {
+  const dispatch = useDispatch();
+
+  const PopupClose=()=>{
+    dispatch({type:'PopupClose'});
+  }
+
+  return (
+    <DangerWrapPopup
+      openStatus={status}
+      style={{backgroundColor:"transparent"}}
+    >
+      <Popup className="spoqaBold" openStatus={true}>
+        <div style={{ fontSize: "1rem" }}>서비스 이용약관</div>
+        <div className="spoqaMedium detail-box">
+            <div className="text">
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+              팝업에 관한 내용이 들어갑니다.
+            </div>
+        </div>
+      </Popup>
+    </DangerWrapPopup>
+  );
+};
+
+const Popup = styled.div`
+  position: absolute;
+
+  top: 40%;
+  left: 50%;
+
+  padding: 1.1875rem 0 2.3125rem 0;
+
+  width: 16.3125rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  transform: translate(-50%, -50%);
+
+  border-radius: 1.3125rem;
+  box-shadow: 0 0 0.25rem 0.0625rem rgba(0, 0, 0, 0.05);
+  background-color: #ffffff;
+
+  text-align: center;
+
+  /* 애니메이션 적용 */
+  transition: opacity 300ms ease-out;
+  opacity: ${(props) => (props.openStatus ? "1" : "0")};
+
+  .detail-box{
+      display: flex;
+      width: 12.9375rem;
+      height: 13.375rem;
+      margin-top: 1rem;
+      border-radius: 0.5625rem;
+      background-color: #f4f4f4;
+  }
+
+  .text{
+      color:#5c5c5c;
+      font-size: 0.75rem;
+      text-align: left;
+      overflow-y: scroll;
+      margin: 1rem;
+  }
+
+  .text::-webkit-scrollbar-thumb{
+        width:0.375rem;
+        height: 5.9375rem;
+        border-radius: 0.1875rem;
+        background-color: #d5d5d5;
+    }
+`;
+
+export default DetailPopup;
